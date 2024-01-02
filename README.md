0fc781b54e896c3ebd5737a20386fe2faedb64b1
gasps <gasps@gasps.com>
2024-01-02 16:43:08 -0500

    Bungeecord upload

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Bungeecord
-Bungeecord with 1.7.x - 1.20.x Support
+BungeeCord for 1.7/1.8/1.9/1.10/1.11/1.12/1.13/1.14/1.15/1.16/1.17/1.18/1.19/1.20
+==========
+This is a fork of md_5's BungeeCord  
+https://www.spigotmc.org/threads/1-8-1-9-bungeecord.392/
+
+This version reimplements Minecraft 1.7.10 and basic 1.7.10 Forge support.
+
+IMPORTANT: There are some bugs but those will take time to fix!
+This fork is designed for keeping your old servers in your network, until your modpacks are available for more recent mc versions.
+Most of them are, so get rid of 1.7 fast and move to newer modpacks asap.
+
+PLEASE: Only use this fork if really needed.(1.7 legacy servers.) If you dont need 1.7 or Sponge support, grab original BungeeCord. Link is above.
+
+Layer 7 proxy designed to link Minecraft servers.
+--------------------------------------------------
+
+BungeeCord is a sophisticated proxy and API designed mainly to teleport players between multiple Minecraft servers. It is the latest incarnation of similar software written by the author from 2011-present.
+
+Information
+-----------
+BungeeCord is maintained by [SpigotMC](https://www.spigotmc.org/) and has its own [discussion thread](https://www.spigotmc.org/go/bungeecord) with plenty of helpful information and links.
+
+### Security warning
+
+As your Minecraft servers have to run without authentication (online-mode=false) for BungeeCord to work, this poses a new security risk. Users may connect to your servers directly, under any username they wish to use. The kick "If you wish to use IP forwarding, please enable it in your BungeeCord config as well!" does not protect your Spigot servers.
+
+To combat this, you need to restrict access to these servers for example with a firewall (please see [firewall guide](https://www.spigotmc.org/wiki/firewall-guide/)).
+
+Source
+------
+Source code is currently available on [GitHub](https://www.spigotmc.org/go/bungeecord-git).
+
+Binaries
+--------
+Precompiled binaries are available for end users on [Jenkins](https://www.spigotmc.org/go/bungeecord-dl).
+
+(c) 2012-2023 SpigotMC Pty. Ltd.
---
+++ protocol/src/main/java/net/md_5/bungee/protocol/packet/PlayerListItem.java
@@ -0,0 +1,167 @@
+package net.md_5.bungee.protocol.packet;
+
+import io.netty.buffer.ByteBuf;
+import java.util.UUID;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import net.md_5.bungee.protocol.AbstractPacketHandler;
+import net.md_5.bungee.protocol.DefinedPacket;
+import net.md_5.bungee.protocol.PlayerPublicKey;
+import net.md_5.bungee.protocol.Property;
+import net.md_5.bungee.protocol.ProtocolConstants;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class PlayerListItem extends DefinedPacket
+{
+
+    private Action action;
+    private Item[] items;
+
+    @Override
+    public void read(ByteBuf buf, ProtocolConstants.Direction direction, int protocolVersion)
+    {
+        if ( protocolVersion < ProtocolConstants.MINECRAFT_1_8 )
+        {
+            items = new Item[ 1 ];
+            Item item = items[ 0 ] = new Item();
+            item.displayName = item.username = readString( buf );
+            action = !buf.readBoolean() ? Action.REMOVE_PLAYER : Action.ADD_PLAYER;
+            item.ping = buf.readShort();
+        } else
+        {
+            action = Action.values()[ DefinedPacket.readVarInt( buf )];
+            items = new Item[ DefinedPacket.readVarInt( buf ) ];
+            for ( int i = 0; i < items.length; i++ )
+            {
+                Item item = items[ i ] = new Item();
+                item.setUuid( DefinedPacket.readUUID( buf ) );
+                switch ( action )
+                {
+                    case ADD_PLAYER:
+                        item.username = DefinedPacket.readString( buf );
+                        item.properties = DefinedPacket.readProperties( buf );
+                        item.gamemode = DefinedPacket.readVarInt( buf );
+                        item.ping = DefinedPacket.readVarInt( buf );
+                        if ( buf.readBoolean() )
+                        {
+                            item.displayName = DefinedPacket.readString( buf );
+                        }
+                        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_19 )
+                        {
+                            item.publicKey = readPublicKey( buf );
+                        }
+                        break;
+                    case UPDATE_GAMEMODE:
+                        item.gamemode = DefinedPacket.readVarInt( buf );
+                        break;
+                    case UPDATE_LATENCY:
+                        item.ping = DefinedPacket.readVarInt( buf );
+                        break;
+                    case UPDATE_DISPLAY_NAME:
+                        if ( buf.readBoolean() )
+                        {
+                            item.displayName = DefinedPacket.readString( buf );
+                        }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void write(ByteBuf buf, ProtocolConstants.Direction direction, int protocolVersion)
+    {
+        if ( protocolVersion < ProtocolConstants.MINECRAFT_1_8 )
+        {
+            Item item = items[0]; // Only one at a time
+            writeString( item.displayName, buf ); // TODO: Server unique only!
+            buf.writeBoolean( action != Action.REMOVE_PLAYER );
+            buf.writeShort( item.ping );
+        } else
+        {
+            DefinedPacket.writeVarInt( action.ordinal(), buf );
+            DefinedPacket.writeVarInt( items.length, buf );
+            for ( Item item : items )
+            {
+                DefinedPacket.writeUUID( item.uuid, buf );
+                switch ( action )
+                {
+                    case ADD_PLAYER:
+                        DefinedPacket.writeString( item.username, buf );
+                        DefinedPacket.writeProperties( item.properties, buf );
+                        DefinedPacket.writeVarInt( item.gamemode, buf );
+                        DefinedPacket.writeVarInt( item.ping, buf );
+                        buf.writeBoolean( item.displayName != null );
+                        if ( item.displayName != null )
+                        {
+                            DefinedPacket.writeString( item.displayName, buf );
+                        }
+                        if ( protocolVersion >= ProtocolConstants.MINECRAFT_1_19 )
+                        {
+                            writePublicKey( item.publicKey, buf );
+                        }
+                        break;
+                    case UPDATE_GAMEMODE:
+                        DefinedPacket.writeVarInt( item.gamemode, buf );
+                        break;
+                    case UPDATE_LATENCY:
+                        DefinedPacket.writeVarInt( item.ping, buf );
+                        break;
+                    case UPDATE_DISPLAY_NAME:
+                        buf.writeBoolean( item.displayName != null );
+                        if ( item.displayName != null )
+                        {
+                            DefinedPacket.writeString( item.displayName, buf );
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void handle(AbstractPacketHandler handler) throws Exception
+    {
+        handler.handle( this );
+    }
+
+    public static enum Action
+    {
+
+        ADD_PLAYER,
+        UPDATE_GAMEMODE,
+        UPDATE_LATENCY,
+        UPDATE_DISPLAY_NAME,
+        REMOVE_PLAYER;
+    }
+
+    @Data
+    public static class Item
+    {
+
+        // ALL
+        UUID uuid;
+
+        // ADD_PLAYER
+        String username;
+        Property[] properties;
+
+        UUID chatSessionId;
+        PlayerPublicKey publicKey;
+
+        // UPDATE_LISTED
+        Boolean listed;
+
+        // ADD_PLAYER & UPDATE_GAMEMODE
+        int gamemode;
+
+        // ADD_PLAYER & UPDATE_LATENCY
+        int ping;
+
+        // ADD_PLAYER & UPDATE_DISPLAY_NAME
+        String displayName;
+
+    }
+}
